52ea4af6d2b03e0768149e0c3e32d2229438fba8
kevlarpixels <kevingaguilar1@gmail.com>
2020-10-18 15:40:29 -0700

    add initil message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hive Management</h1>
+      <p>Photos were taken 10/18/20</p>
+      <p>Page will be live within 24 hours</p>
+      <p>thank you for your patience</p>
     </div>
   );
 }
