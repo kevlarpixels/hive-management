c32044905d7be9c26fc5d2972ae477dd095e7da3
kevlarpixels <kevingaguilar1@gmail.com>
2020-10-19 20:56:00 -0700

    carousel component added

---
+++ src/Carousel.js
@@ -0,0 +1,216 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+export default () => (
+    <Carousel autoPlay>
+        <div>
+            <img src={require('./photos/_DSC4797.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4798.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4799.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4801.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4802.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4803.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4804.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4805.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4806.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4807.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4808.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4809.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4810.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4811.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4812.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4813.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4814.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4815.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4816.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4817.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4818.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4819.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4820.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4821.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4822.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4823.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4824.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4825.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4826.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4827.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+
+        <div>
+            <img src={require('./photos/_DSC4828.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4829.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4830.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4831.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4832.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4833.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4837.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+
+        <div>
+            <img src={require('./photos/_DSC4838.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+        <div>
+            <img src={require('./photos/_DSC4839.jpg')} />
+            <p>Legend 1</p>
+        </div>
+
+
+
+    </Carousel>
+);
+
+
+// Don't forget to include the css in your page
+
+// Using webpack or parcel with a style loader
+// import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+// Using html tag:
+// <link rel="stylesheet" href="<NODE_MODULES_FOLDER>/react-responsive-carousel/lib/styles/carousel.min.css"/
