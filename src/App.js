2078c305c5da240496db27ec49ed7400173ecfcd
kevlarpixels <kevingaguilar1@gmail.com>
2021-04-18 12:50:36 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       <React.Fragment>
         <Router>
           <Switch>
-            <Route exact path="/hive-management" component={Home} />
+            <Route exact path="/" component={Home} />
             <Route path="/hive-management/1055dundas" component={Tenfiftyfive} />
             <Route path="/hive-management/1055dundasandhalf" component={Tenfiftyfivehalf} />
           </Switch>
